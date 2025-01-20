a8ba14c4cd1762e3f925d0f8c40de4ab27a38b48
João Paulo Mazzo <mazzojp@gmail.com>
2025-01-19 21:37:30 -0300

    add amplify config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,10 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
-// import { Amplify } from 'aws-amplify';
-// import awsconfig from '../../amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../../amplify_outputs.json';
 
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
