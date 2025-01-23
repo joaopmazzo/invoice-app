1928adf9b48349b3a05a8e86330726741cb5d045
João Paulo Mazzo <mazzojp@gmail.com>
2025-01-23 01:38:05 -0300

    test deploy backend

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,9 +1,11 @@
-import { referenceAuth } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend';
 
-export const auth = referenceAuth({
-  userPoolId: 'sa-east-1_PaBdaNV54',
-  identityPoolId: 'sa-east-1:82f9dfa8-53c9-4fb4-a46c-1d69cab53658',
-  authRoleArn: 'arn:aws:iam::582274472885:role/service-role/amplify',
-  unauthRoleArn: 'arn:aws:iam::582274472885:role/service-role/amplify',
-  userPoolClientId: '6qetjhj54tk09osmfk2isja7jm',
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
 });
